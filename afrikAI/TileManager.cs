8f3827ecd8a60658d00c36bc510138be3c627ebf
TediBalint <tedi.balint@students.jedlik.eu>
2024-02-19 13:58:59 +0100

    TileManager Basics

--- afrikAI/TileManager.cs
+++ afrikAI/TileManager.cs
@@ -1,6 +1,62 @@
-﻿namespace afrikAI
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace afrikAI
 {
-    internal class TileManager
+    public class TileManager
     {
+        private Tile[,] tiles;
+        private int width;
+        private int height;
+        private string filePath;
+
+        public TileManager(int _width, int _height, string _filePath)
+        {
+            width = _width;
+            height = _height;
+            filePath = _filePath;
+            tiles = new Tile[height, width];
+            readFile();
+        }
+        public void DrawTiles()
+        {
+            foreach (Tile tile in tiles)
+            {
+                tile.DrawTile();
+            }
+        }
+        private void readFile()
+        {
+            if(!File.Exists(filePath)) Debug.WriteLine($"Error In TileManager/readFile: File {filePath} doesn't exist");
+            else
+            { 
+                using(StreamReader sr = new StreamReader(filePath))
+                {
+                    int y = 0;
+                    while(!sr.EndOfStream)
+                    {
+                        string[] data = sr.ReadLine().Trim().Split(' ');
+                        if (data.Length != width) Debug.WriteLine($"Warning in TileManager/readFile file width ({data.Length}) != Tiles width ({width})");
+                        for (int x = 0;x < data.Length ;x++)
+                        {
+                            switch(data[x])
+                            {
+                                case "0":
+                                    // create TileType0 with TileFactory;
+                                    break;
+                                case "1":
+                                    // create TileType1 with TileFactory;
+                                    break;
+                                case "2":
+                                    // create TileType2 with TileFactory;
+                                    break;
+                            }
+                        }
+                        y++;
+                    }
+                    if(y != height) Debug.WriteLine( $"Warning in TileManager/readFile file height ({data.Length}) != Tiles width ({width})")
+                }
+            }
+        }
     }
 }
