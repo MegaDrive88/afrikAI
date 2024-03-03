ca82c1a0207c08098f8cf1197753819a8c2a7672
TediBalint <tedi.balint@students.jedlik.eu>
2024-03-03 23:55:09 +0100

    PathfindingStrategy dp.

---
+++ afrikAI/Pathfinding Modules/DPPathFindingStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace afrikAI.Pathfinding_Modules
+{
+	public class DPPathFindingStrategy : IPathfindingStrategy
+	{
+		private void UpdateTiles(List<Tile> tiles) { }
+		public Vector2[] GetShortestPath(List<Tile> tiles)
+		{
+			UpdateTiles(tiles);
+			return new Vector2[] {new Vector2(1,1), new Vector2(2,2) };
+		}
+	}
+}
---
+++ afrikAI/Pathfinding Modules/PathfindingStrategyContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace afrikAI.Pathfinding_Modules
+{
+	public class PathfindingStrategyContext
+	{
+		private IPathfindingStrategy Strategy;
+		public void ChangeStrategy(IPathfindingStrategy strategy)
+		{
+			Strategy = strategy;
+		}
+
+		
+		public Vector2[] GetShortestPath(List<Tile> tiles)
+		{
+			return Strategy.GetShortestPath(tiles);
+		}
+	}
+}
