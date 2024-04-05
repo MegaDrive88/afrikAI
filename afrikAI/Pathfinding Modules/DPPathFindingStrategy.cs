ead4d7e00adc1cbba45476b3b7f38e2d3e6abda3
TediBalint <tedi.balint@students.jedlik.eu>
2024-04-05 12:16:04 +0200

    Revert "Fixed UpdateTiles"
    This reverts commit ec7d83dbe05bdf15bf1c648a7ca31073848fd71e.

--- afrikAI/Pathfinding Modules/DPPathFindingStrategy.cs
+++ afrikAI/Pathfinding Modules/DPPathFindingStrategy.cs
@@ -57,9 +57,8 @@ namespace afrikAI.Pathfinding_Modules
 			foreach(Tile tile in tiles) tile.ResetDistance(); 
 			shortestLength = 0;
 			List<Vector2> path = new List<Vector2>();
-			UpdateTiles(tiles, endTile); // works? MAY NEED TO REDO!!!
+			for (int i = 0; i < 2; i++) UpdateTiles(tiles, endTile); // works? MAY NEED TO REDO!!!
 			ClosestTileHelper(tiles, startTile, tiles.GetLength(1), tiles.GetLength(0), ref path);
-			Debug.WriteLine(shortestLength);
 			return new TilePath(shortestLength, path);
 		}
 	}
