1b6a71bff71f22b489c9a6780e1462831b3a09bc
MegaDrive88 <podlipnik.adam@students.jedlik.eu>
2024-04-07 13:23:06 +0200

    progress

--- afrikAI/Pathfinding Modules/AdamPathfindingStrategy.cs
+++ afrikAI/Pathfinding Modules/AdamPathfindingStrategy.cs
@@ -4,73 +4,69 @@ namespace afrikAI.Pathfinding_Modules {
     public class AdamPathfindingStrategy : IPathfindingStrategy {
         private int shortestLength = 0;
         List<Vector2> vehtlok = new List<Vector2>();
-        //private static int cOUNTER = 0;
-        private static List<List<Tile>> paths = new List<List<Tile>>();
-        private static bool found;
-        private void MagicHappensHere(Tile[,] tiles, Tile start, Tile goal) {
-            int left = start.x, right = left, top = start.y, bottom = top;
+        private static List<HashSet<Tile>> paths = new List<HashSet<Tile>>();
+        private void MagicHappensHere(Tile[,] tiles, Tile start, Tile goal, ref HashSet<Tile> path) {
+            //Tile zebra;
+            //foreach (Tile tile in tiles) if (tile.TileType == "zebra") zebra = stile;
+            //lassu
             List<int[]> surrounding = new List<int[]> {
                 new[] { start.x, start.y - 1 },
                 new[] { start.x - 1, start.y },
                 new[] { start.x, start.y + 1 },
                 new[] { start.x + 1, start.y }
             };
-            List<Tile> path = new List<Tile> { start };
-            bool found = start.x == goal.x && start.y == goal.y;
-            while (!found && left != 0 && top != 0 && right != tiles.GetLength(1) && bottom != tiles.GetLength(0)) {
-
-                //found = start.x == goal.x && start.y == goal.y;
-
-                foreach (int[] s in surrounding) {
-                    if (new[] { "wall", "lion" }.Contains(tiles[s[1], s[0]].TileType)) {
-                        tiles[s[1], s[0]].Calculated = true;
-                        //break; // return?
-                    }
-                    else if (tiles[s[1], s[0]].TileType == "water") {
-                        //GG
-                    }
-                    else if (tiles[s[1], s[0]].TileType == "ground") {
-
-                        path.Add(tiles[s[1], s[0]]); // kulon path minden groundnak!!
-                        //cOUNTER++;
-                        //MagicHappensHere(tiles, tiles[s[1], s[0]], goal); - rekurzio
-                    }
-                    tiles[s[1], s[0]].Calculated = true;
+            //if (!path.All(x => x.TileType != "water")) return;
+            foreach (int[] s in surrounding) {
+                try {
+                    _ = tiles[s[1], s[0]];
                 }
-                paths.Add(path);
-                shortestLength = paths.Min(x => x.Count);
+                catch {
+                    continue; // szélén van az adott tile a mapnak
+                }
+                Tile curr = tiles[s[1], s[0]];
+                // eddig jo
+
+                if (curr.Calculated) continue;
+                if (new[] { "wall", "lion" }.Contains(curr.TileType)) {
+                    curr.Calculated = true;
+                    continue;
+                    //break;//return;
+                }
+                else if (curr.TileType == "water") {
+                    path.Add(curr);
+                    paths.Add(path);
+                    shortestLength = paths.Min(x => x.Count);
+                    //path = new List<Tile>();
+                    //foreach (Tile t in tiles) t.Calculated = false;
+                    //curr.Calculated = true;
+                    return; // GG
+                }
+                else if (curr.TileType == "ground") {
+                    // ez az ag reworkot igenyel de nagyon
+
+                    // if hashset hossza nagyobb mint width + height - startx - starty : szopo
+
+                    path.Add(curr);
+                    MagicHappensHere(tiles, curr, goal, ref path); // -rekurzio
+                    //path = new List<Tile>();
+                    foreach (Tile t in tiles) t.Calculated = false;
+                    if (paths.Count != 1) return;
+                    //if (paths.Contains(path)) {
+                    //    path = new List<Tile>();
+                    //    foreach (Tile t in tiles) t.Calculated = false;
+                    //}
+                    //if (!path.All(x => x.TileType != "water")) return;
+                }
             }
+            paths.Add(path);
+            shortestLength = paths.Min(x => x.Count);
             
 
-
-
-
-
-
-
-            //for (int i = top; i < tiles.GetLength(0); i++) {
-            //    for(int j = left; j < tiles.GetLength(1); j++) {
-
-            //        //bool toTheRight = goal.x > right;
-            //        //bool toTheTop = goal.y > top;
-            //    }
-            //}
-            //private void drawPath(TilePath path) {
-            //    foreach (Vector2 pos in path.Path) {
-            //        tiles[(int)pos.Y, (int)pos.X].Draw(ConsoleColor.Red);
-            //    }
-            //}
-
-
-
-            //while ((tiles[up, right].TileType != "wall" && tiles[up, left].TileType != "wall") || (tiles[up, right].TileType != "water" && tiles[up, left].TileType != "water")) {
-            //    if (toTheRight) right++; // goofy ahh condition ^
-            //    else left++;
-            //}
         }
         public TilePath GetShortestPath(Tile[,] tiles, Tile startTile, Tile endTile) {
-            MagicHappensHere(tiles, startTile, endTile);
-            paths = new List<List<Tile>>() { paths.Where(x => x.Count == shortestLength).ToList()[0] };
+            HashSet<Tile> sPath = new HashSet<Tile>();
+            MagicHappensHere(tiles, startTile, endTile, ref sPath);
+            paths = new List<HashSet<Tile>>() { paths.Where(x => x.Count == shortestLength).ToList()[0] };
             foreach (Tile t in paths[0]) { // kirajzolas mukodik
                 vehtlok.Add(new Vector2(t.x, t.y));
             }
