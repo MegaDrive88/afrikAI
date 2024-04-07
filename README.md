d98fea502993ed07c3691a8b147b5696aa43495f
MegaDrive88 <podlipnik.adam@students.jedlik.eu>
2024-04-07 22:05:00 +0200

    IT WORKS!!!!!!!!!!!!!!!!!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AfrikAI
 *Készítették:
-Podlipnik Ádám
+Podlipnik Ádám  
 Tédi Bálint*
 
 Az **AfrikAI** egy játékos implementációja több útkereső algoritmusnak. A játékosnak egy zebrának kell segítenie vizet találni egy random generált, vagy akár a játékos által létrehozott sivatagban.
@@ -11,8 +11,14 @@ Az **AfrikAI** egy játékos implementációja több útkereső algoritmusnak. A
 > 1. fal
 > 1. oroszlán
 > 1. víz
+> 1. zebra
 #### A játék célja 
 > A zebrának a falakat és az oroszlánokat kikerülve kell eljutnia a vízhez. Mindeközben, az oroszlánok folyamatosan közelednek felé, szintén egy útkereső algoritmus segítségével.
-> A játékos akár versenyeztethet több algoritmust, létrehozhat saját sivatagot, és végignézheti, hogy a zebra sikerrel jár-e, vagy sem.
-
-to be continued...
+> A játékos létrehozhat saját sivatagot, generáltathat egyet véletlenszerűen, majd segíthet a zebrának vizet találni.
+#### A játék menete
+> A játékos minden körben felcserélhet kétszer két mezőt, amennyiben azok nem állatot vagy vizet tartalmaznak.
+> Az állatok minden körben egy mezőnyit lépnek, a zebra a víz, az oroszlán pedig a zebra felé.
+> Ha a zebra elérte a vizet, a játékos győzött, ám ha az oroszlánok egyike utolérte a zebrát, vége a játéknak.
+#### A szerkesztő működése
+> A szerkesztő megnyitása után a felhasználó egy üres sivatagot talál a képernyőjén, amelyen a nyíl billentyűkkel navigálva a [+] és [-] billentyűk segítségével tudja egy mező típusát megváltoztatni.
+> Mindezek után a [V] gomb segítségével elmenthető a létrehozott pálya.
--- afrikAI/Pathfinding Modules/AdamPathfindingStrategy.cs
+++ afrikAI/Pathfinding Modules/AdamPathfindingStrategy.cs
@@ -5,6 +5,15 @@ namespace afrikAI.Pathfinding_Modules {
         private int shortestLength = 0;
         List<Vector2> vehtlok = new List<Vector2>();
         private static HashSet<HashSet<Tile>> paths = new HashSet<HashSet<Tile>>();
+        private int DistanceHelper(Tile[,] tiles, int[] t1, int[] t2) {
+            int wallcount = 0;
+            for(int i = t1[1] < t2[1] ? t1[1] : t2[1]; i <= (t1[1] < t2[1] ? t2[1] : t1[1]); i++) {
+                for (int j = t1[0] < t2[0] ? t1[0] : t2[0]; j <= (t1[0] < t2[0] ? t2[0] : t1[0]); j++) {
+                    if (tiles[i, j].TileType == "wall") wallcount++;
+                }
+            }
+            return wallcount * 100 / (((t1[1] < t2[1] ? t2[1] - t1[1] : t1[1] - t2[1]) + 1) * ((t1[0] < t2[0] ? t2[0] - t1[0] : t1[0] - t2[0]) + 1));   
+        }
         private void MagicHappensHere(Tile[,] tiles, Tile start, Tile goal, ref HashSet<Tile> path) {
             List<int[]> surrounding = new List<int[]> {
                 new[] { start.x, start.y - 1 },
@@ -12,18 +21,17 @@ namespace afrikAI.Pathfinding_Modules {
                 new[] { start.x, start.y + 1 },
                 new[] { start.x + 1, start.y }
             };
-            List<int[]> temp = new List<int[]>(surrounding);
-            foreach (int[] s in temp) {
+            foreach (int[] s in new List<int[]>(surrounding)) {
                 try {
                     _ = tiles[s[1], s[0]];
+                    tiles[s[1], s[0]].ClosestDistance = DistanceHelper(tiles, s, new[] { goal.x, goal.y });
                 }
                 catch {
                     surrounding.Remove(s);
                 }
             }
+            surrounding = surrounding.OrderBy(x => tiles[x[1], x[0]].ClosestDistance).ToList();
             if (surrounding.Find(x => tiles[x[1], x[0]].TileType == "water") is not null) surrounding[0] = surrounding.Find(x => tiles[x[1], x[0]].TileType == "water");
-            
-            //ennel kicsit komplexebb sortolas kell xd
             foreach (int[] s in surrounding) {
                 Tile curr = tiles[s[1], s[0]];
                 if (curr.Calculated || new[] { "wall", "lion" }.Contains(curr.TileType)) {
@@ -46,7 +54,8 @@ namespace afrikAI.Pathfinding_Modules {
         public TilePath GetShortestPath(Tile[,] tiles, Tile startTile, Tile endTile) {
             HashSet<Tile> sPath = new HashSet<Tile>();
             MagicHappensHere(tiles, startTile, endTile, ref sPath);
-            int shortestPossible = 1 + (startTile.x > endTile.x ? startTile.x - endTile.x : endTile.x - startTile.x) + (startTile.y > endTile.y ? startTile.y - endTile.y : endTile.y - startTile.y);
+            int shortestPossible = (startTile.x > endTile.x ? startTile.x - endTile.x : endTile.x - startTile.x) 
+                                 + (startTile.y > endTile.y ? startTile.y - endTile.y : endTile.y - startTile.y);
             paths = new HashSet<HashSet<Tile>>() { paths.Where(x => x.Count >= shortestPossible).First() };
             foreach (Tile t in paths.First()) { 
                 vehtlok.Add(new Vector2(t.x, t.y));
